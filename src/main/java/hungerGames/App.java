45b8c6dd28966e034ad4d39f41d886c8fd6545cc
giannislevi <44844897+giannislevi@users.noreply.github.com>
2019-10-31 15:32:08 +0200

    Delete print and create weapon class

--- src/main/java/hungerGames/App.java
+++ src/main/java/hungerGames/App.java
@@ -13,6 +13,5 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Eisai malakas.");
     }
 }
