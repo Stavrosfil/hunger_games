f2b9634088018a11164a9521ca21c15c3c454ecc
Stavros Filosidis <stavrosfil@gmail.com>
2019-11-07 01:29:02 +0200

    Fix renaming of App.java

--- src/main/java/hungerGames/Game.java
+++ src/main/java/hungerGames/Game.java
@@ -3,12 +3,13 @@ package hungerGames;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
+public final class Game {
+    private Game() {
     }
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
