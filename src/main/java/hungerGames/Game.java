679098dd92826dce57fa1f06b91c33b6aa86ae6f
Stavros Filosidis <stavrosfil@gmail.com>
2019-12-03 01:54:35 +0200

    Improve data printing

--- src/main/java/hungerGames/Game.java
+++ src/main/java/hungerGames/Game.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public final class Game {
     int round;
+    static int killed = 0;
 
     public Game() {
     }
@@ -27,33 +28,29 @@ public final class Game {
         HeuristicPlayer p1 = new HeuristicPlayer(1, "Yiannis", board, 0, -10, -10, null, null, null, 3);
         Player p2 = new Player(2, "Stavros", board, 0, 10, 10, null, null, null);
 
-        System.out.println(p1.playersDistance(p2));
-        // System.exit(0);
-
-        // Random r = new Random();
-        // int begins = r.nextInt(6) + 1; // Dice roll
-
-        // if (begins % 2 == 0) {
-        // play(p1, p2, board, g);
-        // } else {
-        // play(p2, p1, board, g);
-        // }
+        // System.out.println(p1.playersDistance(p2));
 
         play(p1, p2, board, g);
 
         System.out.print("The hunger games are over! ");
-        if (p1.getScore() > p2.getScore()) {
-            System.out.println(p1.getName() + " is the 2020 winner! With " + p1.getScore() + " points VS "
-                    + p2.getScore() + " points.");
-        } else if (p2.getScore() > p1.getScore()) {
-            System.out.println(p2.getName() + " is the 2020 winner! With " + p2.getScore() + " points VS "
-                    + p1.getScore() + " points.");
+        if (killed == 1) {
+            System.out.println(p1.getName() + " is the 2020 winner!");
+        } else if (killed == 2) {
+            System.out.println(p2.getName() + " is the 2020 winner!");
+        } else {
+            if (p1.getScore() > p2.getScore()) {
+                System.out.println(p1.getName() + " is the 2020 winner! With " + p1.getScore() + " points VS "
+                        + p2.getScore() + " points.");
+            } else if (p2.getScore() > p1.getScore()) {
+                System.out.println(p2.getName() + " is the 2020 winner! With " + p2.getScore() + " points VS "
+                        + p1.getScore() + " points.");
+            }
         }
     }
 
     static void play(HeuristicPlayer p1, Player p2, Board board, Game g) {
         int count = 1;
-        boolean killed = false;
+        killed = 0;
         System.out.println(p1.getName() + " plays first!");
         do {
             g.setRound(count);
@@ -63,14 +60,14 @@ public final class Game {
             p1.statistics();
             if (p1.kill(p1, p2, 2)) {
                 System.out.println(p1.getName() + " killed " + p2.getName());
-                killed = true;
+                killed = 1;
             }
-            if (!killed) {
+            if (killed == 0) {
                 p2.move();
                 System.out.println(p2.getName() + " is now at: (" + p2.getX() + "," + p2.getY() + ")");
                 if (p1.kill(p2, p1, 2)) {
                     System.out.println(p2.getName() + " killed " + p1.getName());
-                    killed = true;
+                    killed = 2;
                 }
             }
             for (String[] i : board.getStringRepresentation()) {
@@ -79,12 +76,14 @@ public final class Game {
                 }
                 System.out.println();
             }
-            if (killed)
-                System.exit(0);
-            if (g.getRound() % 3 == 0)
-                board.resizeBoard(p1, p2);
-            count++;
             System.out.println("--------------------------------------------------------------");
+            if (killed == 0) {
+                if (g.getRound() % 3 == 0)
+                    board.resizeBoard(p1, p2);
+                count++;
+            } else {
+                break;
+            }
         } while (board.getR() > 2);
     }
 
