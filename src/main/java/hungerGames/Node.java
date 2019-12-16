4e1eba2d71b252e8e918309581e8bffe02b153f2
Stavros Filosidis <stavrosfil@gmail.com>
2019-12-16 21:50:36 +0200

    Implement getters and setters

--- src/main/java/hungerGames/Node.java
+++ src/main/java/hungerGames/Node.java
@@ -12,11 +12,11 @@ public class Node {
     double nodeEvaluation;
 
     public Node() {
-        parent = new Node();
-        children = new ArrayList<Node>();
+        parent = null;
+        children = null;
         nodeDepth = -1;
         nodeMove = new int[3];
-        nodeBoard = new Board();
+        nodeBoard = null;
         nodeEvaluation = -1;
     }
 
@@ -30,4 +30,81 @@ public class Node {
         this.nodeEvaluation = nodeEvaluation;
     }
 
+    public Node getParent() {
+        return this.parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public ArrayList<Node> getChildren() {
+        return this.children;
+    }
+
+    public void setChildren(ArrayList<Node> children) {
+        this.children = children;
+    }
+
+    public int getNodeDepth() {
+        return this.nodeDepth;
+    }
+
+    public void setNodeDepth(int nodeDepth) {
+        this.nodeDepth = nodeDepth;
+    }
+
+    public int[] getNodeMove() {
+        return this.nodeMove;
+    }
+
+    public void setNodeMove(int[] nodeMove) {
+        this.nodeMove = nodeMove;
+    }
+
+    public Board getNodeBoard() {
+        return this.nodeBoard;
+    }
+
+    public void setNodeBoard(Board nodeBoard) {
+        this.nodeBoard = nodeBoard;
+    }
+
+    public double getNodeEvaluation() {
+        return this.nodeEvaluation;
+    }
+
+    public void setNodeEvaluation(double nodeEvaluation) {
+        this.nodeEvaluation = nodeEvaluation;
+    }
+
+    public Node parent(Node parent) {
+        this.parent = parent;
+        return this;
+    }
+
+    public Node children(ArrayList<Node> children) {
+        this.children = children;
+        return this;
+    }
+
+    public Node nodeDepth(int nodeDepth) {
+        this.nodeDepth = nodeDepth;
+        return this;
+    }
+
+    public Node nodeMove(int[] nodeMove) {
+        this.nodeMove = nodeMove;
+        return this;
+    }
+
+    public Node nodeBoard(Board nodeBoard) {
+        this.nodeBoard = nodeBoard;
+        return this;
+    }
+
+    public Node nodeEvaluation(double nodeEvaluation) {
+        this.nodeEvaluation = nodeEvaluation;
+        return this;
+    }
 }
